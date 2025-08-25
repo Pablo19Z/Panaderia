044d5e9bcbe1f147b55ef64e72f2f203086f2352
Pablo19Z <juanpablogomezfandino2@gmail.com>
2025-08-25 17:04:16 -0500

    arreglando app

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == '_main_':
-    app.run(host='0.0.0.0', port=5093)
+if __name__ == "__main__":   # corregido
+    app.run(host="0.0.0.0", port=5093)
